b68403d4be92c99bcd832ddb315cc7243c37d8e2
Nestor <nestor.alive@gmail.com>
2023-04-05 15:35:38 -0300

    Primer commit

---
+++ src/components/AddCategory.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from 'react'
+
+export const AddCategory = ({ onNewCategory }) => {
+
+    const [inputValue, setInputValue] = useState('')
+    
+
+    const onInputChange = (event) => {
+        
+        setInputValue(event.target.value)
+    }
+
+    const onSubmit = (event) => {
+        event.preventDefault();
+        if( inputValue.trim().length <= 1 ) return;
+            
+        onNewCategory(inputValue.trim());
+        // setCategories( categories => [ inputValue, ...categories ])
+        setInputValue('')
+    }
+
+  return (
+    <form onSubmit={ onSubmit }>
+        <input
+            type='text'
+            placeholder='Buscar gifs'
+            value={ inputValue }
+            onChange={ onInputChange }
+        /> 
+    </form>
+  )
+}
